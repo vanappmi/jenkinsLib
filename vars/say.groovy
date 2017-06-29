e6bffc1f0259be56c56d7452c9524f2095a54a39
Michael van Appeldorn <michael.vanappeldorn@rewe-digital.com>
2017-06-29 13:28:12 +0200

    test

--- vars/say.groovy
+++ vars/say.groovy
@@ -1,7 +1,14 @@
-def call(String name = 'bla') {
-   echo "hallo"
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
-   pipeline {
-	agent any
-   }
+    // now build, based on the configuration provided
+    node {
+        git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
+        sh "mvn install"
+        mail to: "...", subject: "${config.name} plugin build", body: "..."
+    }
 }
